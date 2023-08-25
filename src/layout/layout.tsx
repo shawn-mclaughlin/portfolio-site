a6f9dd222efd08cdeb1294b3f9b944275ef83c47
Shawn McLaughlin <shawn@shawnmclaughlin.dev>
2023-08-25 10:44:31 -0400

    Adjust image dimensions in intro page

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -8,14 +8,14 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
     return (
         <div className="flex flex-col h-screen">
-            <nav className={"flex justify-between px-6 py-1 bg-sky-700 text-white h-10 items-center"}>
+            <nav className={"flex justify-evenly px-6 py-1 bg-sky-700 text-white h-10 items-center"}>
                 <Link to={"/"}>Shawn McLaughlin</Link>
                 <Link to={"/skills"}>Skills</Link>
                 <Link to={"/career"}>Career</Link>
                 <Link to={"/qualifications"}>Qualifications</Link>
                 <Link to={"/personal"}>Personal</Link>
             </nav>
-            <div className="w-4/5 m-auto h-auto shadow-2xl flex-grow">
+            <div className="w-screen lg:w-10/12 m-auto h-auto shadow-2xl flex-grow">
                 {props.children}
             </div>
         </div>

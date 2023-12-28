aead00f68b7ac070e25a8746514b7cc81a709fb1
Shawn McLaughlin <shawn@shawnmclaughlin.dev>
2023-12-28 17:04:46 -0500

    Updated section title

--- src/skills/skills-section.tsx
+++ src/skills/skills-section.tsx
@@ -69,7 +69,7 @@ export default function SkillsSection() {
 
   return (
     <section id="skills" className="mx-auto w-11/12 max-w-7xl scroll-mt-24 py-4 2xl:w-4/5">
-      <h1 className="mb-4 text-4xl">Skills</h1>
+      <h1 className="mb-4 text-4xl">Skill Set</h1>
       <div className="grid grid-cols-3 place-items-center gap-4 sm:grid-cols-4 lg:grid-cols-6">
         {skills.map((skill, index) => (
           <SkillCard key={index} name={skill.name} icon={skill.icon} />
